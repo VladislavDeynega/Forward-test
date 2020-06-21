ce4010cb0f3ef5140f9de23bc1af07fbec3a65f1
VladislavDeynega <60932821+VladislavDeynega@users.noreply.github.com>
2020-06-21 17:59:06 +0700

    Add files via upload

---
+++ EnginModel/EnginModel/Engine.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace EnginModel
+{
+    class Engine
+    {
+        public event Action<int> Peregrev;
+        public event Action<string> CurrentTdv;
+        public float I { get; set; }
+        public int[] M { get; set; }
+        public int[] V { get; set; }
+
+        public float Tper { get; set; }
+        public float Tsr { get; set; }
+        public float Tdv { get; set; }
+
+        public float Hm { get; set; }
+        public float Hv { get; set; }
+        public float C { get; set; }
+
+        public float Speed = 0;
+
+        public float Vc { get => C * (Tsr - Tdv); }
+
+
+        public Engine(float I, int[] M, int[] V, float Tper, float Hm, float Hv, float C, int Tsr)
+        {
+            this.I = I;
+            this.M = M;
+            this.V = V;
+            this.Tper = Tper;
+            this.Hm = Hm;
+            this.Hv = Hv;
+            this.C = C;
+            this.Tsr = Tsr;
+
+            Tdv = Tsr;
+        }
+
+        public float Vh(int i)
+        {
+            return M[i] * Hm + V[i] * V[i] * Hv;
+        }
+
+        public float Acceleration(int i)
+        {
+            return M[i] / I;
+        }
+
+        private void Simulate()
+        {
+            int sec = 0;
+            int indexM = 0;
+            List<string> curr_Tdv = new List<string>();
+            while(Tdv <= Tper)
+            {                
+                float curr_vh = Vh(indexM);
+
+                Tdv = Tdv + curr_vh - Vc;
+                curr_Tdv.Add(Tdv.ToString());
+                Speed += Acceleration(indexM);
+
+                if (Speed >= V[indexM]) indexM++;
+
+                sec++;               
+                //CurrentTdv?.Invoke(Tdv);
+            }
+            string[] mas_curr_tdv = curr_Tdv.ToArray();
+
+            for (int i = 0; i < mas_curr_tdv.Length; i++)
+            {
+                string sec_preregrev = "°С\tcекунд до перегрева: " + Convert.ToSingle(sec-i-1);
+                mas_curr_tdv[i] += sec_preregrev;
+                Thread.Sleep(200);
+                CurrentTdv?.Invoke(mas_curr_tdv[i]);
+            }
+            Peregrev?.Invoke(sec);
+        }
+
+        public void Start()
+        {
+            Simulate();            
+        }
+    }
+}
---
+++ EnginModel/EnginModel/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnginModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tsr = 0;
+            float I = 10;     
+            int[] M = { 20, 75, 100, 105, 75, 0 };
+            int[] V = { 0, 75, 150, 200, 250, 300 };
+            float Tper = 110;
+            float Hm = 0.01f;
+            float Hv = 0.0001f;
+            float C = 0.1f;
+
+            Console.WriteLine("Взять параметры из файла? y/n");
+            ConsoleKey reply = Console.ReadKey().Key;
+            if (reply == ConsoleKey.Y)
+            {
+                if (Config_from_file("configuration.txt") != null)
+                {
+                    float[] mas_param = Config_from_file("configuration.txt");
+                    I = mas_param[0];
+                    Tper = mas_param[1];
+                    Hm = mas_param[2];
+                    Hv = mas_param[3];
+                    C = mas_param[4];
+                }
+               else
+                {
+                    Console.WriteLine("\nБудут взяты стандартные параметры");
+                    reply = ConsoleKey.N;
+                }
+            }
+            else if (reply != ConsoleKey.N && reply != ConsoleKey.Y)
+            {
+                Console.WriteLine("\nНекорректный ответ");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }           
+          
+            Console.WriteLine("\nВведите значение температуры среды");
+            
+            try 
+            {
+                Tsr = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректное значение \nАвтоматически задано значение 0");           
+            }
+            Engine eng = new Engine(I, M, V, Tper, Hm, Hv, C, Tsr);
+
+            eng.Peregrev += Eng_Peregrev;
+            eng.CurrentTdv += Eng_CurrentTdv;
+            eng.Start();
+            Console.ReadKey();
+        }
+
+        public static float[] Config_from_file(string path)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                string line;
+                List<float> Parameters = new List<float>();
+
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    char equals = '=';
+                    int indexOfChar = line.IndexOf(equals);
+                    line = line.Substring(indexOfChar + 1);
+                    Parameters.Add(Convert.ToSingle(line));
+                }
+                sr.Close();
+                float[] mas = Parameters.ToArray();
+                return mas;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("\nФайл не найден");
+                return null;
+            }            
+            catch (FormatException)
+            {
+                Console.WriteLine("\nНарушена структура данных файла");
+                return null;
+            }
+        }
+        private static void Eng_CurrentTdv(string T)
+        {
+            Console.WriteLine("Температура двигателя: " + T);
+        }
+
+        private static void Eng_Peregrev(int obj)
+        {
+            Console.WriteLine("Со старта двигателя прошло " + obj + " секунд");
+        }
+
+    }
+}
